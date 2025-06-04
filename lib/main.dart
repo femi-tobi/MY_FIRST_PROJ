2066c8b1b396faf05f283aa4c0d8c210662f6636
Adefemi_Oluwatobi <adefemioluwatobi13@gmail.com>
2025-06-04 16:23:08 +0000

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to my App',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+              )),
+          centerTitle: true,
+          backgroundColor: Colors.blue[600],
+        ),
+        body: Center(
+          child: Text('Hello world'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // You can add what happens when clicked here
+            print('You pressed me');
+          },
+          backgroundColor: Colors.blue[600],
+          child: Text(
+            'Press',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
     ));
